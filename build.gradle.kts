0a89762217ed9a862c83869fe84eca84a325877f
Math012 <math111.0811@gmail.com>
2025-09-02 17:52:20 -0300

    Adicionando a dependencia do MapStruct

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.kafka:spring-kafka")
+	// MapStruct
+	implementation("org.mapstruct:mapstruct:1.5.3.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+	//
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
