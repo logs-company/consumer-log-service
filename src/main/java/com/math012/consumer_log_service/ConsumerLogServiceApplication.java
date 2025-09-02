5b0c3b4c297cd1dab8b67e9d816720d8e90ca88c
Math012 <math111.0811@gmail.com>
2025-09-02 17:28:05 -0300

    Primeiro commit

---
+++ src/main/java/com/math012/consumer_log_service/ConsumerLogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.math012.consumer_log_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerLogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerLogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/consumer_log_service/ConsumerLogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.consumer_log_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerLogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
