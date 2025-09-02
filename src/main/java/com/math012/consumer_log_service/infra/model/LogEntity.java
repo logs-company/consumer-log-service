6169ed5ce1c73a80b418c3d5251f7abf5c345d52
Math012 <math111.0811@gmail.com>
2025-09-02 17:45:22 -0300

    Adicionando a entidade Log

---
+++ src/main/java/com/math012/consumer_log_service/infra/model/LogEntity.java
@@ -0,0 +1,27 @@
+package com.math012.consumer_log_service.infra.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class LogEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+    private OffsetDateTime timestamp;
+    private String message;
+}
